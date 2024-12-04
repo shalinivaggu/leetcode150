f55564dda431e84aee579079c156acdfa8d3d027
vaggu shalini <94608109+shalinivaggu@users.noreply.github.com>
2024-12-04 18:58:38 +0530

    Time: 3 ms (6.13%), Space: 8.3 MB (7.98%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int sum = 0 ;
+        while(n > 1) {
+            sum += (n / 5);
+            n = n / 5;
+        }
+        return sum;
+    }
+};
