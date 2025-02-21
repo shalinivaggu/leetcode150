cdc5cc7ca079931f47624520f5f8dac74140bf34
vaggu shalini <94608109+shalinivaggu@users.noreply.github.com>
2025-02-21 22:19:18 +0530

    Time: 3 ms (67.51%), Space: 109.5 MB (30.88%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,42 +11,32 @@
  */
 class Solution {
 public:
-    queue<TreeNode*> q;
     int maxLevelSum(TreeNode* root) {
-        int maxi = root->val, sum = 0, idx = 1;
-        vector<vector<int>> res = levelOrder(root);
+        if(!root) return INT_MIN;
 
-        for (int i = 0; i < res.size(); i++) {
-            int sum = accumulate(res[i].begin(), res[i].end(), 0);
-            if (sum > maxi) {
-                maxi = sum;
-                idx = i + 1;
+        vector<int>ans;
+
+        queue<TreeNode*> pendingNodes; 
+        pendingNodes.push(root);
+
+        while(!pendingNodes.empty()) {
+            int size = pendingNodes.size();
+            int sum = 0; 
+
+            while(size--) {
+                TreeNode* front = pendingNodes.front();
+                pendingNodes.pop();
+
+                sum += front->val ;
+
+                if(front->left) pendingNodes.push(front->left);
+                if(front->right) pendingNodes.push(front->right);
             }
+
+            ans.push_back(sum);
         }
-        return idx;
-    }
 
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if (!root) return result;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int levelSize = q.size();
-            vector<int> level;
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-                level.push_back(node->val);
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-            result.push_back(level);
-        }
-        return result;
+        int max = INT_MIN, maxIndex = 0 ; 
+        return max_element(ans.begin() , ans.end()) - ans.begin() + 1;
     }
 };
