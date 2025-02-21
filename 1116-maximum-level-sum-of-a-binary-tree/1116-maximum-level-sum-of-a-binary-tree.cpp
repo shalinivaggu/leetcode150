e6663498a896ea331814ba2bfa2163b7c1bd2cbc
vaggu shalini <94608109+shalinivaggu@users.noreply.github.com>
2025-02-21 22:12:20 +0530

    Time: 3 ms (67.51%), Space: 115.3 MB (5.22%) - LeetHub

---
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    queue<TreeNode*> q;
+    int maxLevelSum(TreeNode* root) {
+        int maxi = root->val, sum = 0, idx = 1;
+        vector<vector<int>> res = levelOrder(root);
+
+        for (int i = 0; i < res.size(); i++) {
+            int sum = accumulate(res[i].begin(), res[i].end(), 0);
+            if (sum > maxi) {
+                maxi = sum;
+                idx = i + 1;
+            }
+        }
+        return idx;
+    }
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (!root) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> level;
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                level.push_back(node->val);
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            result.push_back(level);
+        }
+        return result;
+    }
+};
