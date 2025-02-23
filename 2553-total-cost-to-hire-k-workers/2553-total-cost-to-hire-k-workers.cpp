d51bdfc11d205756488a672e2246dc54bc4faca8
vaggu shalini <94608109+shalinivaggu@users.noreply.github.com>
2025-02-23 19:14:15 +0530

    Time: 34 ms (83.88%), Space: 76.9 MB (34.9%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
@@ -10,13 +10,11 @@ public:
 
         while(k--) {
             while(pq1.size() < candidates && i <= j) {
-                pq1.push(costs[i]);
-                i++;
+                pq1.push(costs[i++]);
             }
 
             while(pq2.size() < candidates && i <= j) {
-                pq2.push(costs[j]);
-                j--;
+                pq2.push(costs[j--]);
             }
 
             int min1 = pq1.size() > 0 ? pq1.top() : INT_MAX; 
