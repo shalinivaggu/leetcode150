8e8da2dbd76cb1421e8517c9a3d1b02503d8c3c6
vaggu shalini <94608109+shalinivaggu@users.noreply.github.com>
2025-02-23 19:10:46 +0530

    Time: 40 ms (61.28%), Space: 76.9 MB (51.3%) - LeetHub

---
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        priority_queue<int, vector<int> , greater<int>> pq1;
+        priority_queue<int, vector<int> , greater<int>> pq2;
+
+        long long cost = 0 ; 
+        int n = costs.size() ; 
+        int i = 0 , j = n - 1;
+
+        while(k--) {
+            while(pq1.size() < candidates && i <= j) {
+                pq1.push(costs[i]);
+                i++;
+            }
+
+            while(pq2.size() < candidates && i <= j) {
+                pq2.push(costs[j]);
+                j--;
+            }
+
+            int min1 = pq1.size() > 0 ? pq1.top() : INT_MAX; 
+            int min2 = pq2.size() > 0 ? pq2.top() : INT_MAX ; 
+
+            if(min1 <= min2) {
+                cost += min1 ; 
+                pq1.pop();
+            } else {
+                cost+= min2;
+                pq2.pop();
+            }
+        }
+
+        return cost;
+    }
+};
